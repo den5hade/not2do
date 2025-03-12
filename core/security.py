ddefee1fe5c7267c3ca7f4928e6bc6f3560c4969
den5hade <denshade.job@gmail.com>
2025-03-12 12:39:31 +0500

    add try/except con to auth

--- core/security.py
+++ core/security.py
@@ -13,23 +13,36 @@ security = HTTPBasic()
 async def get_current_username(
     credentials: Annotated[HTTPBasicCredentials, Depends(security)],
 ):
-    print(credentials)
-    user = await UserService.get_user_by_telegram_id(credentials.password)
+    try:
+        user = await UserService.get_user_by_telegram_id(credentials.password)
+        if user is None:
+            print("User not found")
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="User not found",
+                headers={"WWW-Authenticate": "Basic"},
+            )
 
-    current_username_bytes = credentials.username.encode("utf8")
-    correct_username_bytes = user.first_name.encode("utf8")
-    is_correct_username = secrets.compare_digest(
-        current_username_bytes, correct_username_bytes
-    )
-    current_password_bytes = credentials.password.encode("utf8")
-    correct_password_bytes = user.telegram_id.encode("utf8")
-    is_correct_password = secrets.compare_digest(
-        current_password_bytes, correct_password_bytes
-    )
-    if not (is_correct_username and is_correct_password):
+        current_username_bytes = credentials.username.encode("utf8")
+        correct_username_bytes = user.first_name.encode("utf8")
+        is_correct_username = secrets.compare_digest(
+            current_username_bytes, correct_username_bytes
+        )
+        current_password_bytes = credentials.password.encode("utf8")
+        correct_password_bytes = user.telegram_id.encode("utf8")
+        is_correct_password = secrets.compare_digest(
+            current_password_bytes, correct_password_bytes
+        )
+        if not (is_correct_username and is_correct_password):
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Incorrect username or password",
+                headers={"WWW-Authenticate": "Basic"},
+            )
+        return str(user.telegram_id)
+    except Exception as e:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username or password",
+            detail="Authentication failed",
             headers={"WWW-Authenticate": "Basic"},
         )
-    return str(user.telegram_id)
